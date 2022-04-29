c0f20e395130c71a920774d8e9ee6af2327fa534
Cyrreal <lazukinki@gmail.com>
2022-04-29 18:59:01 +0300

    fetch

--- js/script.js
+++ js/script.js
@@ -1,62 +1,26 @@
-let getJsonFile = (pathToFile) => {
-  let request = new XMLHttpRequest();
-
-  request.open("GET", pathToFile, false);
-  request.send(null);
-
-  let my_JSON_object = JSON.parse(request.responseText);
-
-  return my_JSON_object;
-};
-const allEmoji = getJsonFile("https://emoji-api-app.herokuapp.com/");
-
-let sortEmoji = allEmoji.map((element) => {
-  for (key in element) {
-    if (key == "keywords") {
-      // console.log(element[key]);
-      let res = element[key].split(" ");
-      res = [...new Set(res)].join(" ");
-      element[key] = res;
-    }
-  }
-  return element;
-});
-
-function loadDivsStart() {
-  let elem = document.querySelector(".grid_main");
-  let insideElems = "";
-  sortEmoji.forEach(function (item) {
-    insideElems += `
-          <div class="grid-item">
-              <div>${item.symbol}</div>
-              <h2>${item.title}</h2>
-              <p>${item.keywords}</p>
-          </div>
-          `;
-  });
-  elem.innerHTML = insideElems;
+let allEmoji;
+const elem = document.querySelector(".grid_main");
+async function postData() {
+  const response = await fetch("https://emoji-api-app.herokuapp.com/");
+  allEmoji = await response.json();
+  console.log(allEmoji);
 }
-loadDivsStart();
-
-let searchInput = document.querySelector("#search");
-searchInput.addEventListener("input", (event) => search(event));
-
-const search = (event) => {
-  let res = sort.filter((elem) => {
-    return (
-      elem.title.includes(event.target.value) ||
-      elem.keywords.includes(event.target.value)
-    );
+postData().then(function () {
+  let sortEmoji = allEmoji.map((element) => {
+    for (key in element) {
+      if (key == "keywords") {
+        // console.log(element[key]);
+        let res = element[key].split(" ");
+        res = [...new Set(res)].join(" ");
+        element[key] = res;
+      }
+    }
+    return element;
   });
+  console.log(sortEmoji);
 
-  loadDivs(res);
-};
-
-function loadDivs(res = []) {
-  let elem = document.querySelector(".grid_main");
-  let insideElems = "";
-
-  if (res.length == 0) {
+  function loadDivsStart() {
+    let insideElems = "";
     sortEmoji.forEach(function (item) {
       insideElems += `
           <div class="grid-item">
@@ -66,7 +30,25 @@ function loadDivs(res = []) {
           </div>
           `;
     });
-  } else {
+    elem.innerHTML = insideElems;
+  }
+
+  let searchInput = document.querySelector("#search");
+  searchInput.addEventListener("input", (event) => search(event));
+  const search = (event) => {
+    let res = sortEmoji.filter((elem) => {
+      return (
+        elem.title.includes(event.target.value) ||
+        elem.keywords.includes(event.target.value)
+      );
+    });
+
+    // sortEmoji(element);
+    elem.innerHTML = "";
+    loadDivs(res);
+  };
+  function loadDivs(res = []) {
+    insideElems = "";
     res.forEach(function (item) {
       insideElems += `
           <div class="grid-item">
@@ -76,7 +58,8 @@ function loadDivs(res = []) {
           </div>
           `;
     });
+
+    elem.innerHTML = insideElems;
   }
-  elem.innerHTML = insideElems;
-}
-loadDivs();
+  loadDivsStart();
+});
